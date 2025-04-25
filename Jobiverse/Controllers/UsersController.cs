977a1956293a434ce58b1fad9826ea60c38cacdd
hochitrung8124 <hochitrung08012004@gmail.com>
2025-04-25 11:34:02 +0700

    Test data first _ getall Users

---
+++ Jobiverse/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Entity.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private JobiverseContext _jobiverseContext;
+
+        public UsersController(JobiverseContext jobiverseContext)
+        {
+            _jobiverseContext = jobiverseContext;
+        }
+
+        [HttpGet]
+        public IActionResult GettAll()
+        {
+            return Ok(_jobiverseContext.Users.ToList());    
+        }
+    }
+}
--- Jobiverse/Program.cs
+++ Jobiverse/Program.cs
@@ -1,10 +1,22 @@
+﻿using Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Add services to the container.
-
+builder.Services.AddDbContext<JobiverseContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("AivenConnection"))
+);
 builder.Services.AddControllers();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); // mặc định là /swagger/index.html
+}
 
 // Configure the HTTP request pipeline.
 
